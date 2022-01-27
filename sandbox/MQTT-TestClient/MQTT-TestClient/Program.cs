6273e706a1c50d13febbf2794e9833e21f9840c1
Sandro Pedrett <sandro.pedrett@hotmail.com>
2022-01-27 18:58:42 +0100

    Added mqtt c# sandbox test

---
+++ sandbox/MQTT-TestClient/MQTT-TestClient/Program.cs
@@ -0,0 +1,69 @@
+﻿// MIT License
+// 
+// Copyright (c) 2022
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Options;
+using MQTTnet.Formatter;
+
+namespace MQTT_TestClient {
+    class Program {
+        private static async Task Main(string[] args) {
+            Console.WriteLine("Hello MQTT Client!");
+
+            var factory = new MqttFactory();
+
+            var optionsBuilder = new MqttClientOptionsBuilder()
+                                 .WithClientId("Test1" + DateTime.UtcNow)
+                                 .WithProtocolVersion(MqttProtocolVersion.V500)
+                                 .WithTcpServer("127.0.0.1", 8883)
+                                 .Build();
+
+            var client = factory.CreateMqttClient();
+            client.UseConnectedHandler(async e => {
+                await client.SubscribeAsync(factory.CreateSubscribeOptionsBuilder()
+                                                   .WithTopicFilter("test/topic")
+                                                   .Build());
+            });
+
+            await client.ConnectAsync(optionsBuilder, CancellationToken.None);
+
+            client.UseApplicationMessageReceivedHandler(h => Console.WriteLine("received: " + Encoding.UTF8.GetString(h.ApplicationMessage.Payload)));
+
+            string text = Console.ReadLine();
+            while (!string.Equals(text, "exit")) {
+                var msg = factory.CreateApplicationMessageBuilder()
+                                 .WithTopic("test/topic")
+                                 .WithPayload(text)
+                                 .Build();
+
+                await client.PublishAsync(msg, CancellationToken.None);
+
+                text = Console.ReadLine();
+            }
+        }
+    }
+}
